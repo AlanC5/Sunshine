862ddda34adf830ec6afe5edde15c757261c7e00
Alan Chen <alanc1995@gmail.com>
2016-04-09 23:28:09 -0400

    Click on weather data opens up Detail.xml

--- app/src/main/java/com/example/alan/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/alan/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.alan.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -100,7 +100,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String forecast = mForecasterAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
 
         });
